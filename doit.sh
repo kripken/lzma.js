21b176d630880a98d8e09508d56f6e93d807472d
Alon Zakai <alonzakai@gmail.com>
2012-03-11 11:30:46 -0700

    more optimizations

--- doit.sh
+++ doit.sh
@@ -4,13 +4,13 @@ cd lzip
 #make lzip -j 2 # native build
 #mv lzip ../lzma-native
 
-make clean
-~/Dev/emscripten/emmake make lzip -j 2 # bitcode build
-mv lzip lzip.bc
+#make clean
+#~/Dev/emscripten/emmake make lzip -j 2 # bitcode build
+#mv lzip lzip.bc
  
 cd ..
 
-~/Dev/emscripten/emcc -O1 --closure 0 lzip/lzip.bc -o lzma.raw.js
+~/Dev/emscripten/emcc -O2 lzip/lzip.bc -o lzma.raw.js
 cat pre.js > lzma.js
 cat lzma.raw.js >> lzma.js
 cat post.js >> lzma.js
